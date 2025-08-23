4a457f6172336dcf734fb2629609b83e67418126
chi-chim-ddi <skwnstkd12@gmail.com>
2025-08-23 23:20:43 +0900

    rlch

---
+++ Trashland Survival/Trashland Survival/Assets/Scripts/Equip/EquipmentData.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+public enum ItemRarity
+{
+    Normal,
+    Rare,
+    Unique,
+    Legendary
+}
+
+[CreateAssetMenu(fileName = "New EquipmentData", menuName = "Data/Equipment Data")]
+public class EquipmentData : ScriptableObject
+{
+    [Header("기본 정보")]
+    public string itemName;
+    [TextArea]
+    public string itemDescription;
+    public Sprite itemIcon;
+    public ItemRarity rarity;
+
+    [Header("능력치 보너스")]
+    public float healthBonus;
+    public float expBonus;
+    public float moveSpeedBonus;
+    public float attackPowerBonus;
+    public float cooldownReduction;
+    public float magnetBonus;
+    public float attackRangeBonus;
+}
